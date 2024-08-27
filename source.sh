453258dd332319210bc45be4cb56eb6d730219e9
purushottamgutthe <purushottamgutthe6767@gmail.com>
2024-08-27 19:41:09 +0530

    Manualy resolved few of the conflicts in the merge

--- source.sh
+++ source.sh
@@ -11,4 +11,18 @@ elif [ ! -f "$inputFile" ]; then
     exit 1
 fi
 
-echo "We are processing $inputFile file for $user!"
+echo "We are processing $inputFile file for $user!"  
+#Execution of the above line means, things are good till now
+
+
+#let's create separate directory for this task and add meaningful lines to it
+dirName="/home/purushottam/Finlyst/Data/$user$(date +%H%M%d%m%Y)"
+mkdir -p "$dirName"
+if [ $? -eq 0 ]; then    #if mkdir is successful then status will be 0
+    echo "file has been creted"
+else
+    echo "Could not create file"
+    exit 1  #flow must be aborted if file is not created
+fi
+
+./fileCreation.sh "$user" "$inputFile" "$dirName"
